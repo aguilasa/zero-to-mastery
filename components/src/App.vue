b01e187dec86b25352a14fda2a6a8f1df5c548cd
Ingmar Schmidt de Aguiar <ingmar.aguiar@gmail.com>
2023-10-25 15:24:26 -0300

    Zero to Mastery Vue

---
+++ components/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import Greeting from "./components/Greeting.vue";
+
+export default {
+  name: "App",
+  components: { Greeting },
+};
+</script>
+
+<template>
+  <p>Hey!</p>
+  <Greeting />
+</template>
